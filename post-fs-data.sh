2c2795fc1d65e0fe1fc42e39f2f22c747ad91df7
Lucian Nicoara <mail.lucian@gmail.com>
2023-05-29 22:23:46 +0300

    test1

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,13 +1,6 @@
 #!/system/bin/sh
-chown root:root /data/adb/modules/GT2RecQualityMod/odm-files/*
-chmod 0644 /data/adb/modules/GT2RecQualityMod/odm-files/*
-chcon -v u:object_r:vendor_configs_file:s0 /data/adb/modules/GT2RecQualityMod/odm-files/*
+chown root:root /data/adb/modules/GT2MinBrightnessMod/mod-files/*
+chmod 0644 /data/adb/modules/GT2MinBrightnessMod/mod-files/*
+chcon -v u:object_r:vendor_configs_file:s0 /data/adb/modules/GT2MinBrightnessMod/mod-files/*
 
-mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/mixer_paths.xml /odm/etc/mixer_paths.xml
-mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/audio_platform_info.xml /odm/etc/audio_platform_info.xml
-
-chown root:root /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
-chmod 0644 /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
-chcon -v u:object_r:vendor_configs_file:s0 /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
-
-mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID4_MIC_Cam.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID4_MIC_Cam.txt
+mount -o bind /data/adb/modules/GT2MinBrightnessMod/mod-files/display_brightness_config_samsung1024.xml /my_product/vendor/etc/display_brightness_config_samsung1024.xml
